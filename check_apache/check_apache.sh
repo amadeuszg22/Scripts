d7a7d8b5e63262a09897e0fa9ed232ac8918e4cc
Amadeusz Grabowski <amadeusz.grabowski@gmail.com>
2014-11-25 12:06:09 +0100

    Prod changes

--- check_apache/check_apache.sh
+++ check_apache/check_apache.sh
@@ -6,17 +6,17 @@ status=$(/etc/init.d/apache2 status)
 check=$(echo "$status" |nawk -F " " 'NR=='1' {print $4}')
 checka=$(curl -I karlik.hyundai.pl| grep HTTP | nawk -F " " 'NR=='1' {print $2}')
 
-
-if [ "$check" == "not" ] || [ "$checka" -ne "200" ]; then
+if [ "$check" == "not" ] || [ "$checka" != "200" ]; then
 echo "$host:Critical!:$status restart in progress"
 echo "$host:$date-Critical!:$status Restart in progress">>/tmp/check_apache.log
 null=$(/etc/init.d/apache2 restart > /dev/null 2>&1)
+sleep 2
 srv=$(/etc/init.d/apache2 status)
 check2=$(echo "$srv" |nawk -F " " 'NR=='1' {print $4}')
 check2a=$(curl -I karlik.hyundai.pl| grep HTTP | nawk -F " " 'NR=='1' {print $2}')
 #echo "$check2"
 	
-	if [ "$check2" == "not" ] || [ "$check2a" -ne "200" ]; then
+	if [ "$check2" == "not" ] || [ "$check2a" != "200" ]; then
 	echo "$host:Critical!:$srv After restart retry"
 	echo "$host:date-Critical!:$srv After restart retry">>/tmp/check_apache.log
         echo "$host:$date-Critical!:$srv After restart retry"| mail -s "$host:Apache Notifier Critical" ama@thedigitals.pl dpacholczyk@gmail.com #ama@thedigitals.pl -r ama@thedigitals.pl
