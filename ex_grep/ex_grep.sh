c7bc74b2840cd8794062035971e6d4c23e1dc35a
Amadeusz Grabowski <amadeusz.grabowski@gmail.com>
2014-12-16 11:37:33 +0100

    ex_grep created

---
+++ ex_grep/ex_grep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+DIR="$1"
+FILE="/tmp/grepsh"
+i=1
+LC=$(ls -1 $DIR|wc -l)
+FN=$(ls -1 $DIR > $FILE)
+
+if [ $# -lt 2 ]
+  then
+    echo "Usage: ex_grep [DIR] [Syntax] "
+  exit 2
+fi
+
+until [ $i = $LC ]; do
+POS=$(cat $FILE |nawk -F ";" 'NR=='$i' {print $1}')
+
+cont=$(grep "$2" $DIR$POS)
+if [ "$cont" != "" ];then
+echo $DIR$POS
+echo -e "\n $cont"
+fi
+i=$[i + 1]
+
+done
