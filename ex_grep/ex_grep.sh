6e1ea8ec3e6274d1b14ddfbceb7af5f06bc12de9
Bartosz Aninowski <bartosz.aninowski@thedigitals.pl>
2014-12-16 12:48:01 +0100

    -c content option switch created

--- ex_grep/ex_grep.sh
+++ ex_grep/ex_grep.sh
@@ -7,19 +7,22 @@ FN=$(ls -1 $DIR > $FILE)
 
 if [ $# -lt 2 ]
   then
-    echo "Usage: ex_grep [DIR] [Syntax] "
+    echo -e"Usage: ex_grep [DIR] [Syntax] [-c]"
+    echo -e"\n -c to show content of files"
   exit 2
 fi
 
 until [ $i = $LC ]; do
 POS=$(cat $FILE |nawk -F ";" 'NR=='$i' {print $1}')
-
 cont=$(grep "$2" $DIR$POS)
 if [ "$cont" != "" ];then
+if [ "$3" == "-c" ];then
 echo $DIR$POS
 echo -e "\n $cont"
+else
+echo $DIR$POS
+fi
 fi
 i=$[i + 1]
-
 done
-##
+
